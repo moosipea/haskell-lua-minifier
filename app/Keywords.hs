bf31257616f69766f43b9a2e0a9c39fde0c334fa
Mattias Volt <mattiasmcvolt@gmail.com>
2023-07-30 00:35:09 +0300

    Lua keywords

---
+++ app/Keywords.hs
@@ -0,0 +1,24 @@
+module Keywords where
+
+data Keyword 
+  = And
+  | Break
+  | Do
+  | Else
+  | Elseif
+  | End
+  | False
+  | For
+  | Function
+  | If
+  | In
+  | Local
+  | Nil
+  | Not
+  | Or
+  | Repeat
+  | Return
+  | Then
+  | True
+  | Until
+  | While
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,6 @@
 module Main where
 
+import qualified Keywords as K
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
